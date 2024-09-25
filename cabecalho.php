a90b022a29b9aea0a711e1d5c3b78bb1f9622d49
Rafael Sena <rafaelsenabarreto@gmail.com>
2024-09-25 16:59:04 -0300

    Orçamento feito

--- cabecalho.php
+++ cabecalho.php
@@ -1,14 +1,39 @@
-<div class="wrapper row0">
-    <nav id="mainav" class="hoc clear">
-        <!-- ################################################################################################ -->
-        <ul class="clear">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li class="active"><a href="">OAB</a></li>
-            <li class="active"><a href="orcamento.php">Orçamento</a></li>
-            <li class="active"><a href="conta_criar.php">Crie sua conta</a></li>
-            <li class="active"><a href="">Pesquise seus dados</a></li>
-         
-        </ul>
-        <!-- ################################################################################################ -->
-    </nav>
-</div>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        
+        <title>Escritório de Advocacia</title>
+        <meta charset="utf-8">
+
+        <!-- REFERÊNCIAS -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+        <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+        <link rel="shortcut icon" href="images/demo/balanca png.png" />
+        <script src="Mascara.js"></script>
+
+
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="estilo.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="wrapper row0">
+            <nav id="mainav" class="hoc clear">
+                <!-- ################################################################################################ -->
+                <ul class="clear">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li class="active"><a href="">OAB</a></li>
+                    <li class="active"><a href="orcamento.php">Orçamento</a></li>
+                    <li class="active"><a href="conta_criar.php">Crie sua conta</a></li>
+                    <li class="active"><a href="">Pesquise seus dados</a></li>
+                 
+                </ul>
+                <!-- ################################################################################################ -->
+            </nav>
+        </div>
+        
+    </body>
